9bd2adff8e25698a9b43d6d33db447ba79c84e7c
Gllanos <87341850+Gllanos@users.noreply.github.com>
2021-07-12 19:04:16 -0500

    Update Documento.java

--- src/Documents/Documento.java
+++ src/Documents/Documento.java
@@ -1,4 +1,29 @@
 package Documents;
 
 public abstract class Documento {
+   private String id_Documento;
+    private Usuario usuario;
+    private String fecha;
+    private Item item;
+
+    public Documento(String id_Documento, Usuario usuario, String fecha, Item item) {
+        this.id_Documento = id_Documento;
+        this.usuario = usuario;
+        this.fecha = fecha;
+        this.item = item;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        return //"Documento{" +
+                "id_Documento='" + id_Documento + '\'' +
+                ", usuario=" + usuario.getNombres()+" " + usuario.getApellidos() +
+                        //", usuario" + usuario+
+                ", fecha='" + fecha + '\'' ;
+               //'}';
+    }
 }
