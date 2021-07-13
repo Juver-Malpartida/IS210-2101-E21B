edb17a664ce4c5aa912ab46f207ae19623824d26
Gllanos <87341850+Gllanos@users.noreply.github.com>
2021-07-12 19:05:34 -0500

    Update DocumentSalida.java

--- src/Documents/DocumentSalida.java
+++ src/Documents/DocumentSalida.java
@@ -1,4 +1,20 @@
 package Documents;
 
 public class DocumentSalida extends Documento{
+      private String cliente;
+
+    public DocumentSalida(String id_Documento, Usuario usuario, String fecha, Item item, String cliente) {
+        super(id_Documento, usuario, fecha, item);
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentSalida{" +
+                super.toString()+
+                ", cliente='" + cliente + '\'' +
+                ", "+getItem()+
+                ", Valor Final= " + getItem().calcularValorSalida()+
+                '}';
+    }
 }
