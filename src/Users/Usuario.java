510f6f26960dc9186eec0ee16564b97cd1f91718
Gllanos <87341850+Gllanos@users.noreply.github.com>
2021-07-12 19:01:13 -0500

    Update Usuario.java

--- src/Users/Usuario.java
+++ src/Users/Usuario.java
@@ -1,8 +1,33 @@
 package Users;
 
 public abstract class Usuario {
-    private int id;
+       private String id;
     private String nombres;
     private String apellidos;
+    private int dni;
+
+    public Usuario(String id, String nombres, String apellidos, int dni) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return 
+                "id='" + id + '\'' +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", dni=" + dni;
+    }
 
 }
