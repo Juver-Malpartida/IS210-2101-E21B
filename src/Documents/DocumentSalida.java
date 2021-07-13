4a238370592caa362e772871d3fd710b51b12cfd
Gllanos <87341850+Gllanos@users.noreply.github.com>
2021-07-13 12:31:50 -0500

    Update DocumentSalida.java

--- src/Documents/DocumentSalida.java
+++ src/Documents/DocumentSalida.java
@@ -4,11 +4,15 @@ import Store.Item;
 import Users.Usuario;
 
 public class DocumentSalida extends Documento{
-      private String cliente;
+    private String cliente;
 
     public DocumentSalida(String id_Documento, Usuario usuario, String fecha, Item item, String cliente) {
         super(id_Documento, usuario, fecha, item);
         this.cliente = cliente;
+    }
+
+    public String getCliente() {
+        return cliente;
     }
 
     @Override
