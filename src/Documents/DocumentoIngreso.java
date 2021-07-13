da1373ca936eec6242381f572f0d3cd921f8f3f1
Gllanos <87341850+Gllanos@users.noreply.github.com>
2021-07-12 19:05:04 -0500

    Update DocumentoIngreso.java

--- src/Documents/DocumentoIngreso.java
+++ src/Documents/DocumentoIngreso.java
@@ -1,4 +1,20 @@
 package Documents;
 
 public class DocumentoIngreso extends Documento{
+      private String proveedor;
+
+    public DocumentoIngreso(String id_Documento, Usuario usuario, String fecha, Item item, String proveedor) {
+        super(id_Documento, usuario, fecha, item);
+        this.proveedor = proveedor;
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentoIngreso{" +
+                super.toString()+
+                ", proveedor='" + proveedor + '\'' +
+                ", "+super.getItem()+
+                ", valor final= " + super.getItem().calcularValorEntrada()+
+                '}';
+    }
 }
