4adcf0d57d43c240644ce0e53afaa6ec9e375289
Gllanos <87341850+Gllanos@users.noreply.github.com>
2021-07-12 19:01:58 -0500

    Update UsuarioAdministrador.java

--- src/Users/UsuarioAdministrador.java
+++ src/Users/UsuarioAdministrador.java
@@ -1,4 +1,14 @@
 package Users;
 
 public class UsuarioAdministrador extends Usuario{
+   public UsuarioAdministrador(String id, String nombres, String apellidos, int dni) {
+        super(id, nombres, apellidos, dni);
+    }
+
+    @Override
+    public String toString() {
+        return "UsuarioAdministrador{" +
+                super.toString()+
+                "}";
+    }
 }
