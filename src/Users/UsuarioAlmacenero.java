a8fccc54c771ca505fdc8345cfdf0145fa959da6
Gllanos <87341850+Gllanos@users.noreply.github.com>
2021-07-12 19:02:29 -0500

    Update UsuarioAlmacenero.java

--- src/Users/UsuarioAlmacenero.java
+++ src/Users/UsuarioAlmacenero.java
@@ -1,4 +1,14 @@
 package Users;
 
 public class UsuarioAlmacenero extends Usuario{
+   public UsuarioAlmacenero(String id, String nombres, String apellidos, int dni) {
+        super(id, nombres, apellidos, dni);
+    }
+
+    @Override
+    public String toString() {
+        return "UsuarioAlmacenero{" +
+                super.toString()+
+                "}";
+    }
 }
